fffaa154288d2eeae5dfd0a669f0f74d65d574b3
Håkon Prestegård <hakonp@gmail.com>
2017-02-11 16:29:10 +0100

    Added config file

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'kult-med-DDE-og-den-slags-da'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'}]
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
