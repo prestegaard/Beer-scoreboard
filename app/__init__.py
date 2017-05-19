08ada0767416f5c49d8dc8c1005d60c0ee68197b
Håkon Prestegård <hakonp@gmail.com>
2017-05-19 16:33:09 +0200

    Some cleanup, and changed run.py to run with socketio instead of app.run

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_openid import OpenID
-from config import basedir
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
@@ -11,9 +8,5 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-lm = LoginManager()
-lm.init_app(app)
-lm.login_view = 'login'
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models, buttons
