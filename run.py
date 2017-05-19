e14fbaec4d1e4771b4142c9cd9951cd66fe20c7f
prestegaard <hakonp@gmail.com>
2017-05-19 20:48:33 +0000

    Commit new database before merge from laptop

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 from app import app, socketio
 
-#app.run(port=5000, host='0.0.0.0', debug=True, threaded=True)
+app.run(port=5000, host='0.0.0.0', debug=True, threaded=True)
 
-socketio.run(app, debug=True)
+#socketio.run(app, debug=True)
