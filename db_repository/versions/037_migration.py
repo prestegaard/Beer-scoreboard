99f8ec97efc5961f742ec27075e5a35b14b9aaa0
prestegaard <hakonp@gmail.com>
2017-05-25 18:14:18 +0000

    New page is now deployed, user testing needed

--- db_repository/versions/037_migration.py
+++ db_repository/versions/037_migration.py
@@ -5,15 +5,27 @@ from migrate import *
 from migrate.changeset import schema
 pre_meta = MetaData()
 post_meta = MetaData()
+user = Table('user', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('name', String(length=64)),
+    Column('nickname', String(length=64)),
+    Column('button_color', String(length=64)),
+    Column('about_me', String(length=140)),
+    Column('img_src', String(length=64)),
+    Column('location', String(length=140)),
+)
+
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
+    post_meta.tables['user'].columns['location'].create()
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
+    post_meta.tables['user'].columns['location'].drop()
