fbe1cc197024209daa861eb7dd2b086caf0376e8
Håkon Prestegård <hakonp@gmail.com>
2017-06-07 21:58:57 +0200

    More Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -b "0.0.0.0:$PORT" app.__init__:app --log-file 
+web: gunicorn -k eventlet run:app --log-file -
--- run.py
+++ run.py
@@ -3,7 +3,6 @@ from app import app, socketio
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 33507))
-	app.run(host='0.0.0.0', debug=True, threaded=True, port=port)
-
-#socketio.run(app, debug=True)
+	#app.run(host='0.0.0.0', debug=True, threaded=True, port=port)
+	socketio.run(app, debug=True, port=port)
 
